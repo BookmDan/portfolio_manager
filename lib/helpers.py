a9379278f240bac233958ae02ad6a6f31c28aa93
BookmDan <danoh329@gmail.com>
2023-11-19 18:33:58 -0800

    pass

--- lib/helpers.py
+++ lib/helpers.py
@@ -84,12 +84,74 @@ def create_employee():
 
 
 def update_employee():
-    pass
+    try:
+        employee_id = input("Enter the employee id: ")
+
+        if not employee_exists_in_database(employee_id):
+            print("Error: Employee not found in the database.")
+            return
+        new_name = input("Enter the new name: ")
+
+        # Prompt for a new job title
+        new_job_title = input("Enter the new job title: ")
+
+        # Prompt for the employee's new department id
+        new_department_id = input("Enter the new department id: ")
+
+        # Update the employee in the database
+        update_employee_in_database(employee_id, new_name, new_job_title, new_department_id)
+
+        # Print a success message
+        print("Employee information updated successfully.")
+
+    except Exception as e:
+        # Print an error message if an exception is thrown
+        print(f"Error: {e}")
 
 
 def delete_employee():
-    pass
+    try:
+        # Prompt for and read in the employee id
+        employee_id = input("Enter the employee id: ")
+
+        # Check if the employee is in the database
+        if employee_exists_in_database(employee_id):
+            # Delete the employee from the database
+            delete_employee_from_database(employee_id)
+
+            # Print a confirmation message
+            print(f"Employee with ID {employee_id} deleted successfully.")
+        else:
+            # Print an error message if the employee is not in the database
+            print("Error: Employee not found in the database.")
+
+    except Exception as e:
+        # Print an error message if an exception is thrown
+        print(f"Error: {e}")
+
 
 
 def list_department_employees():
-    pass
+    try:
+        # Prompt for and read in the department id
+        department_id = input("Enter the department id: ")
+
+        # Find the department with the given id from the database
+        department = get_department_by_id(department_id)
+
+        # Check if the department exists in the database
+        if department:
+            # Get the department's employees using the employees() instance method
+            department_employees = department.employees()
+
+            # Loop to print each employee's data on a separate line
+            for employee in department_employees:
+                print_employee_data(employee)
+
+        else:
+            # Print an error message if the department does not exist in the database
+            print("Error: Department not found in the database.")
+
+    except Exception as e:
+        # Print an error message if an exception is thrown
+        print(f"Error: {e}")
