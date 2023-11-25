20474dc7e6bec90e9a580d77bb85eb577661d20c
BookmDan <danoh329@gmail.com>
2023-11-25 09:30:33 -0800

    create seed for crypto coin portfolio

--- lib/cli.py
+++ lib/cli.py
@@ -1,11 +1,8 @@
 # lib/cli.py
 
-from helpers import (
-    exit_program,
-    helper_1
-)
-from models.crypto_model import CryptoCoin, Portfolio  # Import new models
-from services.coin_gecko_api import CoinGeckoAPI  # Import CoinGecko API
+from helpers import exit_program
+from models.crypto_model import CryptoCoin, Portfolio
+from services.coin_gecko_api import CoinGeckoAPI
 
 def main():
     while True:
@@ -17,6 +14,12 @@ def main():
             helper_1()
         elif choice == "2":
             view_portfolio()
+        elif choice == "3":
+            search_crypto_price()
+        elif choice == "4":
+            buy_crypto()
+        elif choice == "5":
+            sell_crypto()
         else:
             print("Invalid choice")
 
@@ -25,11 +28,67 @@ def crypto_menu():
     print("0. Exit the program")
     print("1. Some useful function")
     print("2. View Portfolio")
+    print("3. Search Crypto Price")
+    print("4. Buy Crypto")
+    print("5. Sell Crypto")
 
-def view_portfolio():
-    # Logic to retrieve and display user's cryptocurrency portfolio
-    # You can interact with the CryptoCoin and Portfolio models and use the CoinGecko API
-    pass
+def buy_crypto():
+    coin_id = input("Enter the crypto ID to buy: ")
+    amount = float(input("Enter the amount to buy: "))
+    
+    # Get current price
+    current_price = CoinGeckoAPI.get_crypto_price(coin_id)
 
-if __name__ == "__main__":
-    main()
+    # Calculate total cost
+    total_cost = amount * current_price
+
+    # Check if the user has enough funds
+    # For simplicity, assuming user has enough funds for now
+    # In a real-world scenario, you'd check the user's balance before proceeding
+
+    # Create a new transaction
+    transaction = Portfolio(coin_id=coin_id, amount=amount, transaction_type='buy', cost=total_cost)
+    
+    # Update the portfolio
+    update_portfolio(transaction)
+
+def sell_crypto():
+    coin_id = input("Enter the crypto ID to sell: ")
+    amount = float(input("Enter the amount to sell: "))
+    
+    # Get current price
+    current_price = CoinGeckoAPI.get_crypto_price(coin_id)
+
+    # Calculate total sale amount
+    total_sale_amount = amount * current_price
+
+    # Check if the user has enough coins to sell
+    # For simplicity, assuming user has enough coins for now
+    # In a real-world scenario, you'd check the user's coin balance before proceeding
+
+    # Create a new transaction
+    transaction = Portfolio(coin_id=coin_id, amount=-amount, transaction_type='sell', cost=total_sale_amount)
+    
+    # Update the portfolio
+    update_portfolio(transaction)
+
+def update_portfolio(transaction):
+    # Logic to update the user's portfolio based on the transaction
+    # For simplicity, assuming that user_id is 1 for now
+    user_id = 1
+    
+    # Retrieve the user's portfolio
+    user_portfolio = Portfolio.query.filter_by(user_id=user_id, coin_id=transaction.coin_id).first()
+
+    if user_portfolio:
+        # Update existing entry
+        user_portfolio.amount += transaction.amount
+        user_portfolio.cost += transaction.cost
+    else:
+        # Create a new entry in the portfolio
+        user_portfolio = Portfolio(user_id=user_id, coin_id=transaction.coin_id,
+                                   amount=transaction.amount, cost=transaction.cost)
+        db.session.add(user_portfolio)
+
+    # Commit the changes
+    db.session.commit()
--- lib/seed.py
+++ lib/seed.py
@@ -1,25 +1,34 @@
-#!/usr/bin/env python3
+from models.crypto_model import CryptoCoin, Portfolio, Transaction
 
-from models.__init__ import CONN, CURSOR
-from models.department import Department
-from models.employee import Employee
+# Drop existing tables
+CryptoCoin.drop_table()
+Portfolio.drop_table()
+Transaction.drop_table()
 
-def seed_database():
-    Employee.drop_table()
-    Department.drop_table()
-    Department.create_table()
-    Employee.create_table()
+# Create new tables
+CryptoCoin.create_table()
+Portfolio.create_table()
+Transaction.create_table()
 
-    # Create seed data
-    payroll = Department.create("Payroll", "Building A, 5th Floor")
-    human_resources = Department.create(
-        "Human Resources", "Building C, East Wing")
-    Employee.create("Amir", "Accountant", payroll.id)
-    Employee.create("Bola", "Manager", payroll.id)
-    Employee.create("Charlie", "Manager", human_resources.id)
-    Employee.create("Dani", "Benefits Coordinator", human_resources.id)
-    Employee.create("Hao", "New Hires Coordinator", human_resources.id)
+# Seed crypto coins
+bitcoin = CryptoCoin.create(name='Bitcoin', symbol='BTC')
+ethereum = CryptoCoin.create(name='Ethereum', symbol='ETH')
+litecoin = CryptoCoin.create(name='Litecoin', symbol='LTC')
 
+# Seed portfolios
+joe_portfolio = Portfolio.create(user_name='Joe', crypto_coin_id=bitcoin.id, amount=0.5)
+bud_portfolio = Portfolio.create(user_name='Bud', crypto_coin_id=ethereum.id, amount=1.2)
+steph_portfolio = Portfolio.create(user_name='Steph', crypto_coin_id=litecoin.id, amount=5.0)
+alice_portfolio = Portfolio.create(user_name='Alice', crypto_coin_id=bitcoin.id, amount=0.8)
+jessica_portfolio = Portfolio.create(user_name='Jessica', crypto_coin_id=ethereum.id, amount=2.5)
+remy_portfolio = Portfolio.create(user_name='Remy', crypto_coin_id=litecoin.id, amount=3.3)
 
-seed_database()
-print("Seeded database")
+# Seed transactions
+Transaction.create(portfolio_id=joe_portfolio.id, transaction_type='buy', amount=0.2, timestamp='2023-11-20T15:00:00')
+Transaction.create(portfolio_id=bud_portfolio.id, transaction_type='buy', amount=0.5, timestamp='2023-11-20T16:30:00')
+Transaction.create(portfolio_id=steph_portfolio.id, transaction_type='sell', amount=1.0, timestamp='2023-11-21T10:45:00')
+Transaction.create(portfolio_id=alice_portfolio.id, transaction_type='buy', amount=0.3, timestamp='2023-11-21T12:15:00')
+Transaction.create(portfolio_id=jessica_portfolio.id, transaction_type='sell', amount=1.2, timestamp='2023-11-22T09:20:00')
+Transaction.create(portfolio_id=remy_portfolio.id, transaction_type='buy', amount=1.5, timestamp='2023-11-22T14:40:00')
+
+print('Seeding complete')
